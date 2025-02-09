eb9b05e7d157aa3ae889c4838ba264de47aaad57
shivamgahuja1999 <shivamgahuja1999@gmail.com>
2025-02-09 22:11:11 +0530

    delete checkmarx card and service and add sonarqube

--- src/Components/pages/SecurityTools.jsx
+++ src/Components/pages/SecurityTools.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SnykCard from '../tools/SnykCard'
-import CheckmarxCard from "../tools/CheckmarxCard";
+import SonarqubeCard from "../tools/SonarqubeCard";
 import TrivyCard from "../tools/TrivyCard";
 import OwaspZapCard from "../tools/OwaspZap";
 
@@ -14,7 +14,7 @@ const SecurityTools = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <SnykCard />
-          <CheckmarxCard />
+          <SonarqubeCard/>
           <TrivyCard />
           <OwaspZapCard/>
         </div>
